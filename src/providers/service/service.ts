11f93898b0a5652e8c9e19075c05ef404a4b94c7
muhammaddwiki <muhammaddwiki126@gmail.com>
2020-06-22 09:08:52 +0700

    uas-appmobbmulti

---
+++ src/providers/service/service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServiceProvider {
+  API_KEY = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=06d3e4823ee241709d748f2b28eccabf';
+
+  constructor(public httpClient: HttpClient) {
+    console.log('Hello ServiceProvider Provider');
+  }
+
+  getNews(){
+    return this.httpClient.get(this.API_KEY);
+  }
+
+}
+
+
+
